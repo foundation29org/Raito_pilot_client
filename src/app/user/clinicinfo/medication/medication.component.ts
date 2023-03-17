66457eb67695c232f8f4e97a99df2bdfca7300f6
f29 javi <javier.logrono@outlook.com>
2023-03-17 18:02:48 +0100

    get dose for patient

--- src/app/user/clinicinfo/medication/medication.component.ts
+++ src/app/user/clinicinfo/medication/medication.component.ts
@@ -145,7 +145,7 @@ export class MedicationComponent implements OnInit, OnDestroy {
   }
 
   getSavedRecommendations() {
-    this.subscription.add( this.http.get(environment.api+'/api/dose/')
+    this.subscription.add( this.http.get(environment.api+'/api/dose/'+ this.authService.getCurrentPatient().sub)
         .subscribe( (resDoses : any) => {
           console.log(resDoses)
             this.savedRecommendations = resDoses;
@@ -460,14 +460,14 @@ export class MedicationComponent implements OnInit, OnDestroy {
       var promDrug = 'I am a doctor. provide general information on the minimum and maximum dose recommended in mg/kg/day for drugs for a patient';
       /*if(this.weight){
         promDrug = promDrug + ' who weighs ' + this.weight + ' kg.';
-      }*/
+      }
       if(this.age!=null){
         if(this.age.years>0){
           promDrug = promDrug + ' who is ' + this.age.years + ' years old';
         }else{
           promDrug = promDrug + ' who is ' + this.age.months + ' months old';
         }
-      }
+      }*/
       promDrug = promDrug + ', and who is taking the following drugs: ';
       var value = { value: promDrug +actualDrugs};
       value.value+=". Use only medical sources. For each drug, returns only numbers, not 'mg/kg/day'. Format of the response: \n\nNameOfTheDrug: [minDose-maxDose]"
@@ -493,9 +493,9 @@ export class MedicationComponent implements OnInit, OnDestroy {
                       min: Math.round(parseFloat(rangeValues[0])*100)/100,
                       max: Math.round(parseFloat(rangeValues[1])*100)/100
                     };
-                    
+                    console.log(this.actualMedications)
                     for (var j = 0; j < this.actualMedications.length; j++) {
-                      if(this.actualMedications[j].drug==nameAndCommercialName[0]){
+                      if(this.actualMedications[j].drug.indexOf(nameAndCommercialName[0])!=-1){
                         this.actualMedications[j].recommendedDose = recommendedDose;
                         this.actualMedications[j].porcentajeDosis = Math.round((this.actualMedications[j].dose / recommendedDose.max) * 100);
                         if(this.actualMedications[j].dose<recommendedDose.min){
